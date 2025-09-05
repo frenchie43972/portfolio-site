e3abb2b1ac039cf4ee5a79497b706fa5a2b593ac
Kristopher French <51432364+frenchie43972@users.noreply.github.com>
2025-09-05 11:14:14 +0900

    Update vite.config.js

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/portfolio-site,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
