5af6533c861715933c768daf496828bed3d67952
Kris French <frenchie43972@gmail.com>
2025-09-10 13:08:13 +0900

    ServicePage icons swapped and styled

--- portfolio-site/src/pages/ServicesPage.vue
+++ portfolio-site/src/pages/ServicesPage.vue
@@ -18,7 +18,6 @@ function getImagePath(filename) {
     <div class="stack-grid">
       <div class="stack-item" v-for="tech in stack" :key="tech.id">
         <img :src="getImagePath(tech.icon)" :alt="tech.name" class="stack-icon" />
-        <span>{{ tech.name }}</span>
       </div>
     </div>
   </section>
@@ -30,7 +29,7 @@ function getImagePath(filename) {
   flex-wrap: wrap;
   gap: 4rem;
   justify-content: center;
-  margin-top: 2rem;
+  /* margin-top: 2rem 0; */
 }
 
 .stack-item {
@@ -41,8 +40,28 @@ function getImagePath(filename) {
 }
 
 .stack-icon {
-  width: 80px; /* control size */
-  height: 80px; /* keeps consistent square */
+  width: 180px; /* control size */
+  height: 180px; /* keeps consistent square */
   object-fit: contain; /* ensures logos don't distort */
+}
+
+@media (max-width: 950px) {
+  .stack-grid {
+    gap: 0;
+  }
+
+  .stack-item {
+    flex: 1 1 45%;
+  }
+}
+
+@media (max-width: 768px) {
+  .stack-grid {
+    gap: 0;
+  }
+  .stack-icon {
+    width: 160px;
+    height: 160px;
+  }
 }
 </style>
